451fe28b40b7f42a21e4434ca28a45bd08395497
AritraChakraborty2003 <aritra.chakraborty203@mail.com>
2026-05-08 13:07:39 +0530

    update in code

--- client/src/app/simulations/gst/e-invoicing-1/invoice/page.tsx
+++ client/src/app/simulations/gst/e-invoicing-1/invoice/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import React from "react";
-import GSTEInvoiceReplica from "../../../../../components/GSTEInvoiceReplica";
+import GSTEInvoiceReplica from "../../../../components/GSTEInvoiceReplica";
 
 export default function GSTEInvoicing1InvoicePage() {
   return <GSTEInvoiceReplica initialScreen="invoice" />;
