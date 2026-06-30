0e8c711d89c0a8967aa645d981db6021eb655a41
AritraChakraborty2003 <aritra.chakraborty203@mail.com>
2026-06-30 20:24:52 +0530

    update in code

---
+++ client/src/app/simulations/epf-reg-1/_test_icons.tsx
@@ -0,0 +1,2 @@
+import { Link2, NotebookText, UserPlus2, ThumbsUp } from "lucide-react";
+export default function T() { return <div>{Link2 as any}{NotebookText as any}{UserPlus2 as any}{ThumbsUp as any}</div>; }
