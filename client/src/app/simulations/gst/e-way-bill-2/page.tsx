473ce86ede9b6979f7f0202d67d94da43e9701af
AritraChakraborty2003 <aritra.chakraborty203@mail.com>
2026-07-22 14:55:59 +0530

    feat: integrate onComplete prop in EWayBill2Page to handle group completion

--- client/src/app/simulations/gst/e-way-bill-2/page.tsx
+++ client/src/app/simulations/gst/e-way-bill-2/page.tsx
@@ -2,7 +2,9 @@
 
 import React from "react";
 import GSTEWayBillDashboard from "@/app/components/GSTEWayBillDashboard";
+import { useSimGroupComplete } from "@/lib/useSimGroupComplete";
 
 export default function EWayBill2Page() {
-  return <GSTEWayBillDashboard />;
+  const notifyGroupComplete = useSimGroupComplete();
+  return <GSTEWayBillDashboard onComplete={notifyGroupComplete} />;
 }
