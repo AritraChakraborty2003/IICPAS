ae60f2924e9e62cc2e2d7680758e7c75e554c3a1
AritraChakraborty2003 <aritra.chakraborty203@mail.com>
2026-07-22 15:03:27 +0530

    feat: add onComplete callback to EWayBill4Page for group completion handling

--- client/src/app/simulations/gst/e-way-bill-4/page.tsx
+++ client/src/app/simulations/gst/e-way-bill-4/page.tsx
@@ -2,8 +2,10 @@
 
 import React from "react";
 import GSTEWayBillReplica from "@/app/components/GSTEWayBillReplica";
+import { useSimGroupComplete } from "@/lib/useSimGroupComplete";
 
 export default function EWayBill4Page() {
+  const notifyGroupComplete = useSimGroupComplete();
   return (
     <GSTEWayBillReplica
       initialScreen="experiment4"
@@ -12,6 +14,7 @@ export default function EWayBill4Page() {
       portalTitle="e-Way Bill Portal"
       companyName="ABC XYZ Private Limited"
       initialShowLaunchScreen={true}
+      onComplete={notifyGroupComplete}
     />
   );
 }
